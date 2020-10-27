7dae951cde40b31fd59b5ee4e10f4cd312f77bcd
DESKTOP-7FJ9QVL\Windows 10 <15691758725@163.com>
2020-10-27 17:41:23 +0800

    添加注释

--- app/src/main/java/com/xilei/xiecheng/repository/BaseRepository.kt
+++ app/src/main/java/com/xilei/xiecheng/repository/BaseRepository.kt
@@ -11,6 +11,12 @@ import kotlinx.coroutines.withContext
 open class BaseRepository {
 
     suspend fun <T : Any> request(call: suspend () -> BaseResp<T>): BaseResp<T> {
+        /**
+         * Kotlin 提供了三个调度器 Dispatchers.MAIN  Dispatchers.IO  Dispatchers.DEFAULT
+         *
+         * 调用 withContext(Dispatchers.IO) 来创建一个在 IO 线程池中运行的块。您放在该块内的任何代码都始终通过 IO 调度器执行。
+         * 由于 withContext 本身就是一个 suspend 函数，它会使用协程来保证主线程安全
+         */
         return withContext(Dispatchers.IO + exceptionHandler) { call.invoke() }
     }
 }
--- app/src/main/java/com/xilei/xiecheng/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/xilei/xiecheng/viewmodel/BaseViewModel.kt
@@ -11,7 +11,22 @@ import kotlinx.coroutines.launch
  */
 open class BaseViewModel : ViewModel(), LifecycleObserver {
 
-    //运行在UI线程的协程
+    /**
+     * 运行在UI线程的协程
+     * CoroutineScope 可以对协程进行追踪，即使协程被挂起也是如此；
+     * CoroutineScope 并不运行协程，它只是确保您不会失去对协程的追踪；
+     * CoroutineScope 会跟踪所有协程，并且可以取消由它所启动的所有协程；
+     * 为了确保所有的协程都会被追踪，Kotlin 不允许在没有使用 CoroutineScope 的情况下启动新的协程。CoroutineScope
+     * 可被看作是一个具有超能力的 ExecutorService 的轻量级版本。它能启动新的协程，同时这个协程还具备我们在第一部分
+     * 所说的 suspend 和 resume 的优势；
+     *
+     * 调用launch创建并启动协程 ：构建器适合执行 "一劳永逸" 的工作，意思就是说它可以启动新协程而不将结果返回给调用方；
+     * 您可以将 launch 看作是将代码从常规函数送往协程世界的桥梁。在 launch 函数体内，您可以调用 suspend 函数并能够像我们上一篇介绍的那样保证主线程安全
+     */
+
+    /**
+     * 通过使用 viewModelScope，可以确保所有的任务，包含死循环在内，都可以在不需要的时候被取消掉
+     */
     fun lunchUi(block: suspend CoroutineScope.() -> Unit) = viewModelScope.launch {
         block()
     }
